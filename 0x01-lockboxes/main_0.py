83a1c5986173e1e50e654c7ddd4603cc09734c29
BilalEnenouisser <167254302+BilalEnenouisser@users.noreply.github.com>
2024-07-30 14:27:31 +0100

    add

--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -9,5 +9,5 @@ boxes = [[1, 4, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+print(canUnlockAll(boxes)) 
 
